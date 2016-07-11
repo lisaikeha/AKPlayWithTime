e6dca742f694fdb57118e17f119e15c7acfb4765
lisaike <lisaikeha@gmail.com>
2016-07-11 22:30:22 +0800

    Initial Commit

---
+++ AKPlayWithTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKPlayWithTime
+//
+//  Created by lisaike on 16/7/11.
+//  Copyright © 2016年 lisaike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AKPlayWithTime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AKPlayWithTime
+//
+//  Created by lisaike on 16/7/11.
+//  Copyright © 2016年 lisaike. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
